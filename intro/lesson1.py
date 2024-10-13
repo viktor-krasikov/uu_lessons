6c2a202f6f7edd951d214b09f6224affab016443
Viktor Krasikov <viktor.krasikov@gmail.com>
2024-10-13 19:53:57 +0800

    Практическое задание по уроку "Базовые структуры данных"

---
+++ intro/lesson1.py
@@ -0,0 +1,16 @@
+# 1st program
+print((9 ** 0.5) * 5)
+
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd program
+a = 2 * 2 + 2
+b = 2 * (2 + 2)
+print(a)
+print(b)
+print(a == b)
+
+# 4th program
+s = '123.456'
+print(int(float(s) * 10) % 10)
