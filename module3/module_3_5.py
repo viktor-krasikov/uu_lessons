f5c3f7646f730210e3fa79975c475ffcd019d443
Viktor Krasikov <viktor.krasikov@gmail.com>
2024-11-05 02:39:23 +0800

    fix Задача "Рекурсивное умножение цифр"

--- module3/module_3_5.py
+++ module3/module_3_5.py
@@ -4,8 +4,14 @@ def get_multiplied_digits(number):
         return int(str_number)
     else:
         first = int(str_number[0])
-        return first * get_multiplied_digits(int(str_number[1:]))
+        return first * get_multiplied_digits(int(str_number[1:])) or first
 
 
 result = get_multiplied_digits(40203)
-print(result)
+print(result)  # 24
+result = get_multiplied_digits(420)
+print(result)  # 8
+result = get_multiplied_digits(10)
+print(result)  # 1
+result = get_multiplied_digits(0)
+print(result)  # 0
