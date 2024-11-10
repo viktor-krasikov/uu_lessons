f13fafdaf91ef96ea64fb3fd6a5acbcb6ad497ca
Viktor Krasikov <viktor.krasikov@gmail.com>
2024-11-10 22:50:57 +0800

    д/з Добавил метод из 5_1

--- module5/module_5_2.py
+++ module5/module_5_2.py
@@ -3,6 +3,13 @@ class House:
         self.name = name
         self.number_of_floors = number_of_floors
 
+    def go_to(self, new_floor):
+        if 1 <= new_floor <= self.number_of_floors:
+            for i in range(1, new_floor + 1):
+                print(i)
+        else:
+            print("Такого этажа не существует")
+
     def __len__(self):
         return self.number_of_floors
 
