acf60d7aefdff5badcd126dd98aa41c8805b4de5
Viktor Krasikov <viktor.krasikov@gmail.com>
2024-11-30 03:07:28 +0800

    доп д/з по модулю 7 по теме "Режимы открытия файлов"

---
+++ module7/module_7_1.py
@@ -0,0 +1,45 @@
+class Product:
+    def __init__(self, name, weight, category):
+        self.name = name
+        self.weight = weight
+        self.category = category
+
+    def __str__(self):
+        return f"{self.name}, {self.weight}, {self.category}"
+
+
+class Shop:
+    def __init__(self):
+        self.__file_name = 'products.txt'
+
+    def get_products(self):
+        try:
+            with open(self.__file_name, 'r') as file:
+                return file.read()
+        except FileNotFoundError:
+            return None
+
+    def add(self, *products):
+        products_data = self.get_products()
+        extract_product_name = lambda row: row.split(', ')[0]
+        existing_names = set(map(extract_product_name, products_data.split('\n'))) if products_data else set()
+
+        for product in products:
+            if product.name in existing_names:
+                print(f"Продукт {product.name} уже есть в магазине")
+            else:
+                with open(self.__file_name, 'a') as file:
+                    file.write(str(product) + '\n')
+                # existing_names.add(product.name)
+
+
+s1 = Shop()
+p1 = Product('Potato', 50.5, 'Vegetables')
+p2 = Product('Spaghetti', 3.4, 'Groceries')
+p3 = Product('Potato', 5.5, 'Vegetables')
+
+print(p2)  # __str__
+
+s1.add(p1, p2, p3)
+
+print(s1.get_products())
