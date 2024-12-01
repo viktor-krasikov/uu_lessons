9f8b3bc19d23250bc980642580899b97480a4d83
Viktor Krasikov <viktor.krasikov@gmail.com>
2024-12-01 21:30:46 +0800

    доп д/з по модулю 7 по теме "Позиционирование в файле"

---
+++ module7/module_7_2.py
@@ -0,0 +1,22 @@
+def custom_write(file_name, strings):
+    strings_positions = {}
+
+    with open(file_name, 'w', encoding='utf-8') as file:
+        for index, string in enumerate(strings, start=1):
+            byte_position = file.tell()
+            file.write(string + '\n')
+            strings_positions[(index, byte_position)] = string
+
+    return strings_positions
+
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+]
+
+result = custom_write('test.txt', info)
+for elem in result.items():
+    print(elem)
