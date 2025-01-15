aee5a227ffac5f895a0c6ed25a373d8cd8e7982c
Viktor Krasikov <viktor.krasikov@gmail.com>
2025-01-16 06:13:33 +0800

    д/з по модулю 13 по теме "Машина состояний"

---
+++ module13/module_13_4.py
@@ -0,0 +1,60 @@
+import os
+
+from aiogram import Bot, Dispatcher, executor
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+
+TOKEN = os.getenv("TESTING_BOT")
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+
+@dp.message_handler(text=["Calories"])
+async def set_age(message):
+    await message.answer("Введите свой возраст:")
+    await UserState.age.set()
+
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message, state):
+    try:
+        await state.update_data(age=float(message.text))
+    except Exception as e:
+        await message.answer('Возраст должен быть числом. Введите заново:')
+        return
+    await message.answer('Введите свой рост:')
+    await UserState.growth.set()
+
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message, state):
+    try:
+        await state.update_data(growth=float(message.text))
+    except Exception as e:
+        await message.answer('Рост должен быть числом. Введите заново:')
+        return
+    await message.answer('Введите свой вес:')
+    await UserState.weight.set()
+
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message, state):
+    try:
+        await state.update_data(weight=float(message.text))
+    except Exception as e:
+        await message.answer('Вес должен быть числом. Введите заново:')
+        return
+    data = await state.get_data()
+    calories = 10 * data['weight'] + 6.25 * data['growth'] - 5 * data['age'] + 5
+    await message.answer(f'Ваша норма калорий: {calories} ккал.')
+    await state.finish()
+
+
+executor.start_polling(dp, skip_updates=True)
